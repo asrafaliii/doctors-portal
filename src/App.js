d41153fc69b731ad82fdc7db01120002946c9a34
asrafaliii <asraf.bookaand@gmail.com>
2022-05-10 09:47:13 +0600

    daisy ui to use default navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route paht="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
