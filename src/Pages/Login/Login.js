8a4408c369b697cb07ad4d6d7b1df932f0836176
asrafaliii <asraf.bookaand@gmail.com>
2022-05-12 12:41:28 +0600

    handle login error and display loading spinner

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
+import Loading from '../Shared/Loading';
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+
+    let signInError;
+
+    if(loading || gLoading){
+        return <Loading />
+    }
+    if(error || gError){
+        signInError = <p className='text-red-500'><small>{error?.message || gError?.message}</small></p>
+    }
     
-    if(user) {
-        console.log(user);
+    if(gUser) {
+        console.log(gUser);
     }
 
     const onSubmit = data =>{
         console.log(data);
+        signInWithEmailAndPassword(data.email, data.password)
     } 
     
     return (
@@ -72,7 +89,7 @@ const Login = () => {
   </label>
 </div>
 
-      
+      {signInError}
       <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
     </form>
 
