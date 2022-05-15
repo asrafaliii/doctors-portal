fed1cb7d3272239ca27003502d1fd43820c91719
asrafaliii <asraf.bookaand@gmail.com>
2022-05-15 15:53:28 +0600

    react query to update slots automatically

--- src/Pages/Appointment/AvailableAppoint.js
+++ src/Pages/Appointment/AvailableAppoint.js
@@ -1,25 +1,35 @@
 import { format } from 'date-fns';
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 import BookingModal from './BookingModal';
 import Service from './Service';
 
 const AvailableAppoint = ({date}) => {
-    const [services, setServices] = useState([]);
+    // const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
 
     const formattedDate = format(date, 'PP');
+    const {data: services, isLoading, refetch} = useQuery(['available', formattedDate], () => fetch(`http://localhost:5000/available?date=${formattedDate}`)
+    .then(res => res.json()))
 
+
+    if(isLoading){
+        <Loading></Loading>
+    }
+    /*
     useEffect( () =>{
         fetch(`http://localhost:5000/available?date=${formattedDate}`)
         .then(res => res.json())
         .then(data => setServices(data));
-    },[])
+    },[formattedDate])
+    */
     return (
         <div className='my-10'>
             <h3 className='text-xl text-secondary text-center'>Available Appointments is on {format(date, 'PP')}</h3>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    services.map(service=><Service
+                    services?.map(service=><Service
                     key={service._id}
                     service={service}
                     setTreatment={setTreatment}
@@ -30,6 +40,7 @@ const AvailableAppoint = ({date}) => {
             date={date} 
             treatment={treatment}
             setTreatment={setTreatment}
+            refetch={refetch}
             >
             </BookingModal>}
         </div>
