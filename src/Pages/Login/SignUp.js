8d059ce348bab2a16aa5caee657ad02160d02ea9
asrafaliii <asraf.bookaand@gmail.com>
2022-05-16 11:21:42 +0600

    registerd user information in the database

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
 
 const SignUp = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -18,8 +19,11 @@ const SignUp = () => {
       ] = useCreateUserWithEmailAndPassword(auth);
 
       const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+      const [token] = useToken(user || gUser);
       
       const navigate = useNavigate();
+
     let signInError;
 
     if(loading || gLoading || updating){
@@ -29,15 +33,16 @@ const SignUp = () => {
         signInError = <p className='text-red-500'><small>{error?.message || gError?.message}</small></p>
     }
     
-    if(user || gUser) {
-        console.log(user || gUser);
+    if(token) {
+        
+        navigate('/appointment')
     }
 
     const onSubmit = async data =>{
         await createUserWithEmailAndPassword(data.email, data.password)
         await updateProfile({ displayName: data.name });
         console.log("update done");
-        navigate('/appointment')
+        
     } 
     return (
         <div className='flex h-screen justify-center items-center'>
