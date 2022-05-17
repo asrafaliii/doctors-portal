a940e81eac412bbf276378dfdac9f64382c72c17
asrafaliii <asraf.bookaand@gmail.com>
2022-05-17 12:52:57 +0600

    dashboard features based on admin

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const UserRow = ({user}) => {
-    console.log(user);
-    const {email} = user;
+const UserRow = ({user, refetch}) => {
+    // console.log(user);
+    const {email, role} = user;
+
+    const makeAdmin = () =>{
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers:{
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => {
+            if(res.staut === 403){
+                toast.error('Failed to make and admin')
+            }
+            return res.json()})
+        .then(data =>{
+
+            if(data.modifiedCount > 0) {
+                refetch()
+                toast.success(`Successfuly made an admin`);
+
+            }
+            
+        })
+    }
     return (
         <tr>
             <th>1</th>
             <td>{email}</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
+        <td>{role !== 'admin' && <button onClick={makeAdmin} class="btn btn-xs">Make Admin</button>}</td>
+            <td><button class="btn btn-xs">Remove User</button></td>
       </tr>
     );
 };
